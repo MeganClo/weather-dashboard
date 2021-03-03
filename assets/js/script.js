0f47ced45e4aed47ac15e1258026f4160c57c417
Megs <Megs.close@gmail.com>
2021-03-02 17:31:12 -0800

    getting form set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // getting city weather
-var getCityWeather = function(city) {
+var getCurrentCityWeather = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=da47b6e8a518f806d11e4e81cc84a11f";
 
     //make request to the url
@@ -11,4 +11,18 @@ var getCityWeather = function(city) {
     });
 };
 
-getCityWeather("London");
+// getting forcast
+var getCityForcast = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=40294c6c026c149fa301c764eea53d3a";
+
+    //make request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getCurrentCityWeather("London");
+getCityForcast("London");
