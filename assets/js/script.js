5bf407c71f7f65e97d9c88fe372c6460d82ac822
Megs <Megs.close@gmail.com>
2021-03-04 10:51:06 -0800

    getting some html and pulling data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,23 @@ var getCurrentCityWeather = function(city) {
     fetch(apiUrl).then(function(response) {
         console.log(response);
         response.json().then(function(data) {
-            console.log(data);
+            displayWeather(data, city);
         }); 
     });
 };
 
 // getting forcast
-//var getCityForcast = function(city) {
-//    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7&appid=40294c6c026c149fa301c764eea53d3a";
+var getCityForcast = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=da47b6e8a518f806d11e4e81cc84a11f";
 
-    //make request to the url
-//    fetch(apiUrl).then(function(response) {
-//        console.log(response);
-//        response.json().then(function(data) {
-//            console.log(data);
-//        });
-//    });
-//};
+ //   make request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
 
 // targetting the form
 var weatherFormEl = document.getElementById("city-form");
@@ -44,8 +44,13 @@ var submitButton = function(event) {
     }
 };
 
+var displayWeather = function(weather, searchTerm) {
+    console.log(weather);
+    console.log(searchTerm);
+}
+
 weatherFormEl.addEventListener("submit", submitButton);
 
 
 getCurrentCityWeather("London");
-//getCityForcast("London");
+getCityForcast("London");
