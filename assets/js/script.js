3db19592f59b4da7d081c93503e703844a2d0ee5
Megs <Megs.close@gmail.com>
2021-03-11 11:49:30 -0800

    yay! figured out how to send the button text content to the functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,7 @@ var getCityForcast = function(city) {
 //        console.log(response);
         response.json().then(function(data) {
             console.log(data);
+            displayForcast(data, city);
         });
     });
 };
@@ -52,6 +53,10 @@ var getCityForcast = function(city) {
 var submitButton = function(event) {
     event.preventDefault();
     var cityName = cityInputEl.value.trim();
+    var btn = document.createElement("button");
+    btn.className =  "city-buttons";
+    btn.innerHTML = cityName;
+    buttons.appendChild(btn);
     loadCities();
     if (!cities.includes(cityName) && (cityName != "")) {
         cities.push(cityName);
@@ -77,12 +82,33 @@ var displayWeather = function(weather, searchTerm) {
     console.log(searchTerm);
 };
 
+var displayForcast = function(forcast, searchTerm) {
+    cityName.textContent = searchTerm;
+    console.log(forcast);
+    console.log(forcast.city.name);
+    console.log(forcast.list[1].main.temp);
+    console.log(forcast.list[1].main.humidity);
+    console.log(forcast.list[1].dt_txt);
+    console.log(forcast.list[1].weather[0].icon);
+
+};
+
 var createButtons = function() {
     for (var i = 0; i < cities.length; i++) {
         console.log(cities[i]);
         var btn = document.createElement("button");
+        btn.className = "city-buttons"
         btn.innerHTML = cities[i];
         buttons.appendChild(btn);
+    };
+    // targetting the city buttons
+    var cityButtons = document.querySelectorAll(".city-buttons");
+    console.log(cityButtons);
+    for (var i =0; i < cityButtons.length; i ++) {
+        cityButtons[i].addEventListener("click", function(event) {
+            getCurrentCityWeather(event.target.textContent);
+            getCityForcast(event.target.textContent);
+        })
     }
 };
 
@@ -91,6 +117,9 @@ weatherFormEl.addEventListener("submit", submitButton);
 
 loadCities();
 createButtons();
+
+
 console.log(cities);
 //getCurrentCityWeather("London");
 //getCityForcast("London"); 
+// var iconurl = https://openweathermap.org/img/w/ + iconcode + .png;
